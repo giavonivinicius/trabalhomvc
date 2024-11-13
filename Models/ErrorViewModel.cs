7da2b9bc2575e9699b0cf25decdb7ba897f5cb85
Giavoni Vinicius <vini.giavoni@gmail.com>
2024-11-13 11:17:13 -0300

    codigo limpo v1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CodigoLimpoMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
